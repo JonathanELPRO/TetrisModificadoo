92a779cc6865a56fdcf22b92f89f62f962c27453
chunguslol <agustin.ledezma@ucb.edu.bo>
2025-03-17 18:48:39 -0400

    Se Corrige todos los errores de Move this if to a new line or add the missing else.

--- assets/js/tetris.js
+++ assets/js/tetris.js
@@ -3,18 +3,18 @@
 
 
 const SHAPES = [
-    [ 1632                   ],
-    [ 8738, 3840, 17476, 240 ],
-    [ 610 ,  114,   562, 624 ],
-    [ 802 , 1136,   550, 113 ],
-    [ 1570, 116 ,   547, 368 ],
-    [ 561 , 864 ,  1122,  54 ],
-    [ 306 , 1584,   612,  99 ]
+    [1632],
+    [8738, 3840, 17476, 240],
+    [610, 114, 562, 624],
+    [802, 1136, 550, 113],
+    [1570, 116, 547, 368],
+    [561, 864, 1122, 54],
+    [306, 1584, 612, 99]
 ]
 
 
-const COLORS= []
-COLORS = [ "#111", "#00a300", "#9f00a7", "#603cba", "#ffc40d", "#ee1111", "#99b433", "#ff0096"];
+const COLORS = []
+COLORS = ["#111", "#00a300", "#9f00a7", "#603cba", "#ffc40d", "#ee1111", "#99b433", "#ff0096"];
 
 
 let dw = 360;
@@ -28,34 +28,34 @@ class Tetris extends Loop {
         this.score = 0;
         this.level = 1;
         this.currentShape = null;
-        this.time     = 0;
-        this.keyLeft  = new Key(0.15);
+        this.time = 0;
+        this.keyLeft = new Key(0.15);
         this.keyRight = new Key(0.15);
-        this.keyDown  = new Key(0.20);
-        this.keyUp    = new Key();
+        this.keyDown = new Key(0.20);
+        this.keyUp = new Key();
         super(); //
     }
 
     create = function (parent) {
         this.canvas = document.createElement("canvas");
-        this.canvas.style.width  = "100%";
+        this.canvas.style.width = "100%";
         this.canvas.style.height = "100%";
         parent.appendChild(this.canvas);
 
-        window.addEventListener('keydown', this.inputHandler.bind(this),false);
-        window.addEventListener('keyup'  , this.inputHandler.bind(this),false);
-        
+        window.addEventListener('keydown', this.inputHandler.bind(this), false);
+        window.addEventListener('keyup', this.inputHandler.bind(this), false);
+
         let self = this;
-        let resize = function() {
-        	self.canvas.width  = dw;
-        	self.canvas.height = dh;
-        }      
+        let resize = function () {
+            self.canvas.width = dw;
+            self.canvas.height = dh;
+        }
         window.onresize = resize;
         resize();
         this.running = true;
-        
+
         this.restart();
-        this.start(function(dt) {
+        this.start(function (dt) {
             self.update(dt);
             self.render(self.canvas.getContext("2d"));
         }, 60);
@@ -92,135 +92,142 @@ class Tetris extends Loop {
         }
     }
 
-    inputHandler = function(e) {
+    inputHandler = function (e) {
 
         let code = e.keyCode;
         let down = e.type == "keydown";
         let prevent = false;
-        switch(code) {
+        switch (code) {
             default:
                 break;
             case 32:
-                if(this.gameOver && !down) {
+                if (this.gameOver && !down) {
                     this.restart();
                 }
                 prevent = true;
                 break;
             case 37:
-                if(down && this.keyLeft.isReleased()) {
+                if (down && this.keyLeft.isReleased()) {
                     this.keyLeft.setState(JUST_PRESSED);
-                } if(!down) {
+                }
+                if (!down) {
                     this.keyLeft.setState(RELEASED);
                 }
                 prevent = true;
                 break;
             case 39:
-                if(down && this.keyRight.isReleased()) {
+                if (down && this.keyRight.isReleased()) {
                     this.keyRight.setState(JUST_PRESSED);
-                } if(!down) {
+                }
+                if (!down) {
                     this.keyRight.setState(RELEASED);
                 }
                 prevent = true;
                 break;
             case 40:
-                if(down && this.keyDown.isReleased()) {
+                if (down && this.keyDown.isReleased()) {
                     this.keyDown.setState(JUST_PRESSED);
-                } if(!down) {
+                }
+                if (!down) {
                     this.keyDown.setState(RELEASED);
                 }
                 prevent = true;
                 break;
             case 38:
-                if(down && this.keyUp.isReleased()) {
+                if (down && this.keyUp.isReleased()) {
                     this.keyUp.setState(JUST_PRESSED);
-                } if(!down) {
+                }
+                if (!down) {
                     this.keyUp.setState(RELEASED);
                 }
                 prevent = true;
                 break;
         }
-        if(prevent) e.preventDefault();
+        if (prevent) e.preventDefault();
     }
 
-    update = function(deltaTime) {
+    update = function (deltaTime) {
 
-        if(this.gameOver) {            
+        if (this.gameOver) {
             return;
-        } if (this.currentShape == null || this.currentShape.remove) {
-						
-			let lines = this.grid.checkLines();
-			if (lines > 0) {
-				this.score += lines * 100;
-				if (this.score > this.level * 1000) {
-					this.level++;
-				}
-			}
-	
-			let shapeId = Math.floor(Math.random() * 7);
-			this.currentShape = new Shape(SHAPES[shapeId], shapeId + 1, this.grid);
-			this.currentShape.x = 3;
-			this.time = 0;
-			
-			if (!this.currentShape.canMove(this.currentShape.x, this.currentShape.y)) { // GAME OVER
+        }
+        if (this.currentShape == null || this.currentShape.remove) {
+
+            let lines = this.grid.checkLines();
+            if (lines > 0) {
+                this.score += lines * 100;
+                if (this.score > this.level * 1000) {
+                    this.level++;
+                }
+            }
+
+            let shapeId = Math.floor(Math.random() * 7);
+            this.currentShape = new Shape(SHAPES[shapeId], shapeId + 1, this.grid);
+            this.currentShape.x = 3;
+            this.time = 0;
+
+            if (!this.currentShape.canMove(this.currentShape.x, this.currentShape.y)) { // GAME OVER
                 this.gameOver = true;
                 return;
-			}
-						
-		}
-        
-		if (this.time > 1) {
-			this.time = 0;
-			this.currentShape.moveDown();
-		} else {
-			this.time += deltaTime * this.level;
-		}
-				
-		if (this.keyLeft.isJustPressed() || this.keyLeft.isHoldDown()) {
+            }
+
+        }
+
+        if (this.time > 1) {
+            this.time = 0;
+            this.currentShape.moveDown();
+        } else {
+            this.time += deltaTime * this.level;
+        }
+
+        if (this.keyLeft.isJustPressed() || this.keyLeft.isHoldDown()) {
             this.keyLeft.setState(PRESSED);
-			this.currentShape.moveLeft();
-		} if(this.keyLeft.isPressed()) {
-			this.keyLeft.addHoldDownTime(deltaTime * this.level);
-		}
-		
-		if (this.keyRight.isJustPressed() || this.keyRight.isHoldDown()) {
+            this.currentShape.moveLeft();
+        }
+        if (this.keyLeft.isPressed()) {
+            this.keyLeft.addHoldDownTime(deltaTime * this.level);
+        }
+        if (this.keyRight.isJustPressed() || this.keyRight.isHoldDown()) {
             this.keyRight.setState(PRESSED);
-			this.currentShape.moveRight();
-		} if(this.keyRight.isPressed()) {
-			this.keyRight.addHoldDownTime(deltaTime * this.level);
-		}
-				
-		if (this.keyDown.isJustPressed() || this.keyDown.isHoldDown()) {
-			this.keyDown.setState(PRESSED);
-			this.time = 0;
-			this.currentShape.moveDown();
-		}if(this.keyDown.isPressed()) {
-			this.keyDown.addHoldDownTime(deltaTime * 10 *  this.level);
-		}if (this.keyUp.isJustPressed()) {
-            this.keyUp.setState(Key.PRESSED);
-			this.currentShape.rotateRight();
-		}
+            this.currentShape.moveRight();
+        }
+        if (this.keyRight.isPressed()) {
+            this.keyRight.addHoldDownTime(deltaTime * this.level);
+        }
+        if (this.keyDown.isJustPressed() || this.keyDown.isHoldDown()) {
+            this.keyDown.setState(PRESSED);
+            this.time = 0;
+            this.currentShape.moveDown();
+        }
+        if (this.keyDown.isPressed()) {
+            this.keyDown.addHoldDownTime(deltaTime * 10 * this.level);
+        }
         if (this.keyUp.isJustPressed()) {
             this.keyUp.setState(Key.PRESSED);
-			this.currentShape.rotateRight();
-		}
-        
+            this.currentShape.rotateRight();
+        }
+        if (this.keyUp.isJustPressed()) {
+            this.keyUp.setState(Key.PRESSED);
+            this.currentShape.rotateRight();
+        }
+
     }
 
-    render = function(ctx) {
-    	
-        ctx.fillStyle = "#040404"; // black
-		ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-		
-        this.grid.render(ctx);
-        if(this.currentShape != null) this.currentShape.render(ctx);
-		ctx.font = '20px sans-serif';
-		ctx.fillStyle = "#808080"; // gray
-		ctx.fillText("Score: " + this.score, 10, dh - 15);
-		ctx.fillText("Level: " + this.level, dw - 80, dh - 15);
+    render = function (ctx) {
 
-        if(this.gameOver) {   
+        ctx.fillStyle = "#040404"; // black
+        ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.grid.render(ctx);
+        if (this.currentShape != null) this.currentShape.render(ctx);
+        ctx.font = '20px sans-serif';
+        ctx.fillStyle = "#808080"; // gray
+        ctx.fillText("Score: " + this.score, 10, dh - 15);
+        ctx.fillText("Level: " + this.level, dw - 80, dh - 15);
+
+        if (this.gameOver) {
             let text = "Press Space";
-            let dimensions = ctx.measureText(text);             
+            let dimensions = ctx.measureText(text);
             ctx.fillText(text, dw * 0.5 - dimensions.width * 0.5, dh * 0.5 + 30);
             ctx.font = 'bold 25px sans-serif';
             text = "GAME OVER";
@@ -228,13 +235,13 @@ class Tetris extends Loop {
             ctx.fillText(text, dw * 0.5 - dimensions.width * 0.5, dh * 0.5);
         }
 
-    } 
+    }
 
-    dispose = function() {
+    dispose = function () {
         this.stop();
     }
 
-    restart = function() {
+    restart = function () {
         this.grid = new Grid(10, 20);
         this.currentShape = null;
         this.level = 1;
